e959bcc7e4bfa8f786dc2451cca72d82bf5e8c2b
t3team-laptop <t3teamad@gmail.com>
2025-01-14 10:04:05 -0600

    initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,148 @@
+/*
+ * Hey future T3 :) Before I leave I just wanted to thank 
+ * this team for giving me the experiences it did.
+ * I had no idea I was going to fall in love with making
+ * things out of my own hands and watching them come to life.
+ * But robotics showed me that, and I will forever be grateful
+ * for it. I hope that as you guys code, build, and design
+ * for this season you enjoy it as much as I always did. 
+ * I know it's cheesy but please don't forget to stop 
+ * and smell the roses. Remember why you are doing what you 
+ * are doing. Even though things suck when stuff doesn't go
+ * as planned, it's a part of the learning process and it 
+ * makes you a smarter person as you adapt to it. I know we
+ * can't always win and that's okay. You're learning. And that's
+ * what matters. I don't think I can even imagine a high school 
+ * experience without T3 in it. I devoted 4 years of my life to 
+ * this crap and I have absolutely no regrets. As someone who has 
+ * been programming for the team for the past few years I know
+ * that it can be alot sometimes, but don't worry. Just do your
+ * best with the knowledge you have. Also, I wouldn't have done
+ * anything other than be the programmer for this team and I
+ * loved that I did it. Thank you to all of the people that made
+ * my robotics experiences so much better by just being in it.
+ * Special shoutouts to: my favorite co-captains Anshu and Ryan,
+ * Carrie my favorite coding partner, Anika the little sister
+ * I've always wanted, and last but most certainly not least, 
+ * Mr. Ware and Mr.Garren for enabling me to have such an
+ * amazing educational experience with robotics. 
+ * 
+ * Thank you for everything,
+ * Akshita Santra 
+ */
+
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import frc.robot.commands.Swerve.*;
+import frc.robot.subsystems.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+    /* Controllers */
+    private final CommandXboxController baseDriver = new CommandXboxController(0);
+    private final CommandXboxController armDriver = new CommandXboxController(1);
+
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    //private final JoystickButton zeroGyro = new JoystickButton(baseDriver, XboxController.Button.kY.value);
+    //private final JoystickButton robotCentric = new JoystickButton(baseDriver, XboxController.Button.kLeftBumper.value);
+
+    /* Subsystems */
+    private final Swerve s_Swerve = new Swerve();
+
+    /* Commands */
+    private final SlowMode slowMode;
+    private final FastMode fastMode;
+
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        CameraServer.startAutomaticCapture();
+        s_Swerve.setDefaultCommand(
+            new TeleopSwerve(
+                s_Swerve, 
+                () -> -baseDriver.getRawAxis(translationAxis), 
+                () -> -baseDriver.getRawAxis(strafeAxis), 
+                () -> -baseDriver.getRawAxis(rotationAxis), 
+                () -> baseDriver.leftBumper().getAsBoolean()
+            )
+        );
+
+        /*groundIntake.setDefaultCommand(
+            new ManualPivotIntake(
+                groundIntake, 
+               () -> armDriver.getRawAxis(translationAxis)));
+*/
+        slowMode = new SlowMode(s_Swerve);
+        slowMode.addRequirements(s_Swerve);
+        fastMode = new FastMode(s_Swerve);
+        fastMode.addRequirements(s_Swerve);
+
+        
+
+
+        // Configure the button bindings
+        configureButtonBindings();
+        
+      
+/* 
+        SmartDashboard.putData("On-the-fly path", Commands.runOnce(() ->{
+            Pose2d currentPose = s_Swerve.getPose();
+
+            Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
+            Pose2d endPos = new Pose2d(currentPose.getTranslation().plus(new Translation2d(2,0)), new Rotation2d());
+            List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
+
+            PathPlannerPath path = new PathPlannerPath(
+            bezierPoints,
+             new PathConstraints(3, 3, 2*Math.PI, 4*Math.PI),
+             new GoalEndState(0, Rotation2d.fromDegrees(0))
+            );
+            path.preventFlipping = true;
+
+            AutoBuilder.followPath(path).schedule();
+        }));
+        */
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() { 
+        baseDriver.y().onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
+         baseDriver.b().onTrue(slowMode);
+         baseDriver.b().onTrue(fastMode);
+      }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return null;
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,183 @@
+package frc.robot.subsystems;
+
+import frc.robot.SwerveModule;
+import frc.robot.Constants;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Swerve extends SubsystemBase {
+    public SwerveDriveOdometry swerveOdometry;
+    public SwerveModule[] mSwerveMods;
+    public Pigeon2 gyro;
+    public static Field2d odometry;
+    public double speedMultiplier;
+
+    public Swerve() {
+
+        odometry = new Field2d();
+        gyro = new Pigeon2(Constants.Swerve.pigeonID, "cani");
+        gyro.getConfigurator().apply(new Pigeon2Configuration());
+        gyro.setYaw(180);
+        speedMultiplier = 1.0;
+
+        mSwerveMods = new SwerveModule[] {
+            new SwerveModule(0, Constants.Swerve.Mod0.constants),
+            new SwerveModule(1, Constants.Swerve.Mod1.constants),
+            new SwerveModule(2, Constants.Swerve.Mod2.constants),
+            new SwerveModule(3, Constants.Swerve.Mod3.constants)
+        };
+
+        swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions());
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+
+        SwerveModuleState[] swerveModuleStates =
+            Constants.Swerve.swerveKinematics.toSwerveModuleStates(fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                                    translation.getX() * speedMultiplier, 
+                                    translation.getY() * speedMultiplier, 
+                                    rotation * speedMultiplier, 
+                                    getHeading()
+                                )
+                                : new ChassisSpeeds(
+                                    translation.getX() * speedMultiplier, 
+                                    translation.getY() * speedMultiplier, 
+                                    rotation * speedMultiplier));
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+        }
+    }    
+
+    //trying slow mode - maybe take out
+    public void setSpeedMultiplier(double speedMultiplier){
+        this.speedMultiplier = speedMultiplier;
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds(){
+        return Constants.Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds){
+        SwerveModuleState[] states = Constants.Swerve.swerveKinematics.toSwerveModuleStates(speeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.AutoConstants.kMaxSpeedMetersPerSecond);
+        setModuleStates(states);
+    }
+
+
+    /* Used by SwerveControllerCommand in Auto */
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.AutoConstants.kMaxSpeedMetersPerSecond);
+        
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+        }
+    }
+
+    public SwerveModuleState[] getModuleStates(){
+        SwerveModuleState[] states = new SwerveModuleState[4];
+        for(SwerveModule mod : mSwerveMods){
+            states[mod.moduleNumber] = mod.getState();
+        }
+        return states;
+    }
+
+    public SwerveModulePosition[] getModulePositions(){
+        SwerveModulePosition[] positions = new SwerveModulePosition[4];
+        for(SwerveModule mod : mSwerveMods){
+            positions[mod.moduleNumber] = mod.getPosition();
+        }
+        return positions;
+    }
+
+    public Pose2d getPose() {
+        return swerveOdometry.getPoseMeters();
+    }
+
+    public void setPose(Pose2d pose) {
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
+    }
+    
+    public void resetOdometry(Pose2d pose) {
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
+    }
+
+    public Rotation2d getHeading(){
+        return getPose().getRotation();
+    }
+    
+
+    public void setHeading(Rotation2d heading){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), heading));
+    }
+
+    public void zeroHeading(){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
+    }
+
+    public void autoHeadingFix(){
+          var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+            if(alliance.get() == DriverStation.Alliance.Red){
+                double angle = getHeading().getDegrees() > 0? getHeading().getDegrees()-180 : getHeading().getDegrees()+180 ;
+                setHeading(new Rotation2d(Units.degreesToRadians(angle)));
+             }
+         }
+        
+    }
+
+    public Rotation2d getGyroYaw() {
+        return Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble());
+    }
+
+    public void resetModulesToAbsolute(){
+        for(SwerveModule mod : mSwerveMods){
+            mod.resetToAbsolute();
+        }
+    }
+
+    public void setX(){
+
+      mSwerveMods[0].setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)),true);
+      mSwerveMods[1].setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(315.0)),true);
+      mSwerveMods[2].setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(315.0)),true);
+      mSwerveMods[3].setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)),true);
+
+    }
+
+    
+
+    @Override
+    public void periodic(){
+        odometry.setRobotPose(swerveOdometry.getPoseMeters());
+        swerveOdometry.update(getGyroYaw(), getModulePositions());
+
+
+        SmartDashboard.putData("field", odometry);
+
+        for(SwerveModule mod : mSwerveMods){
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " CANcoder", mod.getCANcoder().getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", mod.getPosition().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond); 
+            SmartDashboard.putBoolean("slow mode", speedMultiplier == .5);    
+            SmartDashboard.putNumber("heading", getHeading().getDegrees());
+        }
+    }
+}
